2ba99499413a7131e541e208ee4f558b1fb00f7d
Gurkirpal Singh <sgurkirpal88@gmail.com>
2021-06-13 17:40:00 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,28 @@
+function typeWriter() {
+    var sam = l[i];
+    
+    if (j < sam.length && ww==0) {
+        document.getElementById("type_here").innerHTML += sam.charAt(j);
+        j++;
+        if(j==sam.length){
+            setTimeout(typeWriter,600);
+            return;
+        }
+        setTimeout(typeWriter, 100);
+        return;
+    }
+    if (j == sam.length) {
+        ww = 1;
+    }
+    if (ww == 1) {
+        document.getElementById("type_here").innerHTML = document.getElementById("type_here").innerHTML.slice(0, -1);
+        j--;
+        if(j==-1){
+            ww=0;
+            i=(i+1)%4;
+        }
+    
+        setTimeout(typeWriter, 50);
+        return;
+    }
+}
