2fdb4ddfe84ad4fe3dbc4b14843ed1c29a69d1fb
Gurkirpal Singh <sgurkirpal88@gmail.com>
2021-06-14 20:06:49 +0530

    Updated

--- README.md
+++
@@ -1,1 +0,0 @@
-# sgurkirpal.github.io
--- main.js
+++ main.js
@@ -1,11 +1,19 @@
+$(document).ready(function() {
+
+    setTimeout(function() {
+        $('body').addClass('loaded');
+    }, 3000);
+
+});
+
 function typeWriter() {
     var sam = l[i];
-    
-    if (j < sam.length && ww==0) {
+
+    if (j < sam.length && ww == 0) {
         document.getElementById("type_here").innerHTML += sam.charAt(j);
         j++;
-        if(j==sam.length){
-            setTimeout(typeWriter,600);
+        if (j == sam.length) {
+            setTimeout(typeWriter, 600);
             return;
         }
         setTimeout(typeWriter, 100);
@@ -17,11 +25,11 @@ function typeWriter() {
     if (ww == 1) {
         document.getElementById("type_here").innerHTML = document.getElementById("type_here").innerHTML.slice(0, -1);
         j--;
-        if(j==-1){
-            ww=0;
-            i=(i+1)%4;
+        if (j == -1) {
+            ww = 0;
+            i = (i + 1) % 4;
         }
-    
+
         setTimeout(typeWriter, 50);
         return;
     }
